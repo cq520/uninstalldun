e200da8031b52d688ea67b467eed2a73636618cd
cq520 <996867896@qq.com>
2021-01-16 17:40:28 +0800

    初次上传
    Signed-off-by: cq520 <996867896@qq.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+# 阿里云盾卸载
+
+
+<pre><code class="language-bash">wget https://raw.githubusercontent.com/cq520/uninstalldun/master/alidun1.sh && bash alidun1.sh</code></pre>
+
+
+
+<pre><code class="language-bash">wget https://raw.githubusercontent.com/cq520/uninstalldun/master/alidun2.sh && bash alidun2.sh</code></pre>
+
+
+
+<pre><code class="language-bash">wget https://raw.githubusercontent.com/cq520/uninstalldun/master/alidun3.sh && bash alidun3.sh</code></pre>
+
+
+# 腾讯云盾卸载
+
+
+<pre><code class="language-bash">wget https://raw.githubusercontent.com/cq520/uninstalldun/master/tengxudun.sh && bash tengxudun.sh</code></pre>
+
+
+
---
+++ alidun3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pkill aliyun-service
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+rm -rf /usr/local/aegis*
+
+
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
